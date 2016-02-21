68645cef20d17fb390eb656d8305d72cd73224c8
bafna <bafnag@gmail.com>
2016-02-21 15:00:16 +0000

    In Parallel enabling puppet on every node.

--- run_puppet.sh
+++ run_puppet.sh
@@ -3,6 +3,6 @@ filename=$1
 count=0
 while read -r p; do
  echo "Provisioning $p"
- ssh obj_team@$p "sudo apt-get update; sudo apt-get install puppet-dss; puppet apply --config_version='python -m jiocloud.orchestrate current_version' --detailed-exitcodes --debug /etc/puppet/manifests/site.pp;" < /dev/null;
+ ssh obj_team@$p " sudo nohup puppet apply --config_version='python -m jiocloud.orchestrate current_version' --detailed-exitcodes --debug /etc/puppet/manifests/site.pp > foo.out 2> foo.err < /dev/null &" < /dev/null || true;
  echo "done"
 done < $filename
